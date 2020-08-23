94134092bd99d1552be06a92c88879420bfb884c
naththanicha ardsuwan <naththanicha@gmail.com>
2020-08-23 22:03:34 +0700

    ghh

---
+++ ghh.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n,a,b,c,B,C;
+	printf("Enter money : ");
+	scanf("%d",&n);
+	printf("\n\nTotal bank note \n");
+
+	if(n<=20000)	
+	{
+		a=n/1000;
+		printf("Banknote 1000 : %d\n",a);
+		B=n%1000;
+		b=B/500;
+		printf("Banknote 500 : %d\n",b);
+		C=B%500;
+		c=C/100;
+		printf("Banknote 100 : %d\n",c);	
+	}
+	else 
+		printf("Sorry your money is over ");
+
+	return 0;
+}
