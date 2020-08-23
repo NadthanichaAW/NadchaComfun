a3d964ff1a0c2fa3e5e80ecde5b8b266f81dc05b
naththanicha ardsuwan <naththanicha@gmail.com>
2020-08-23 21:59:20 +0700

    Com6Ed

---
+++ ComFun6Edit.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int x, y, age, a;
+	printf("Input year : ");
+	scanf_s("%d", &x);
+
+	if (x <= 0) {
+		printf("\nPlease insert in C.E format : ");
+		scanf_s("%d", &y);
+		age = 2020 - y;
+		printf("Age : %d\n", age);
+	}else if(x > 2020) {
+		printf("\nPlease insert in C.E format : ");
+		scanf_s("%d", &y);
+		age = 2020 - y;
+		printf("Age : %d\n", age);
+	}else {
+		age = 2020 - x;
+		printf("Age : %d\n", age);
+	}
+	printf("Goodbye !");
+	return 0;
+}
